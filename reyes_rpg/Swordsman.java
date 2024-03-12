da71d9bdf3c84cf222d094cbdd882d373e0238e3
HertzelReyes <160230185+HertzelReyes@users.noreply.github.com>
2024-03-12 20:14:47 +0800

    Add files via upload

---
+++ reyes_rpg/Swordsman.java
@@ -0,0 +1,17 @@
+
+package reyes_rpg;
+
+
+
+    public class Swordsman extends Character {
+    
+    public Swordsman(int health, int damage, int shield) {
+        super(health, damage, shield);
+        
+    }
+    public void receiveDamage(int damage){
+        super.receiveDamage(damage);
+        }
+    
+    }
+
